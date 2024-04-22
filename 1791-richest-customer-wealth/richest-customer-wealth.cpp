6de678fdd23889cd79ae1e88ff0b93abe66efa85
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2024-04-22 17:13:56 +0530

    Time: 3 ms (78.89%) | Memory: 10.4 MB (14.42%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -9,12 +9,14 @@ public:
         for(int i=0;i<accounts.size();i++)
         {
             int temp = 0;
+            
             for(int j=0;j<accounts[i].size();j++)
             {
                temp+= accounts[i][j];
-               
             }
-            if(temp > wealth){
+
+            if(temp > wealth)
+            {
                 wealth = temp;
             }
 
