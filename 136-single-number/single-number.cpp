fcbf516e6a51a3c549e5f955518d490fbd818737
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2024-08-07 14:49:09 +0530

    Time: 525 ms (5.02%) | Memory: 19.6 MB (26.89%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int n = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            bool found = false;
+            for (int j = 0; j < nums.size(); j++)
+            {
+                if (i != j && nums[i] == nums[j])
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                n = nums[i];
+                break;
+            }
+        }
+        return n;
+        
+    return n; 
+    }
+};
