c69725d7b17c257202c010381f76d60d401e3944
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2024-08-08 16:13:19 +0530

    Time: 13 ms (6.28%) | Memory: 13.7 MB (12.40%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int thirdMax(vector<int>& nums) {
+    int thirdMax(vector<int>& nums) 
+    {
         int count = 0;
         int ans = 0;
         int max = 0, secmax = 0, thrmax = 0;
+
         set<int> uniqueNums(nums.begin(), nums.end());
-        // uniqueNums[2]
+    
+
         vector<int> uniqueNumsArr;
         int n = uniqueNums.size();
+
         auto it = uniqueNums.begin();
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++) 
+        {
             uniqueNumsArr.push_back(*it);
             it++;
         }
+
         count=uniqueNumsArr.size();
+
         sort(uniqueNumsArr.begin(), uniqueNumsArr.end(), greater<int>());
+
         ans = uniqueNumsArr[2];
 
-        if (count < 3) {
-            for (int i = 0; i < nums.size(); i++) {
-                if (max < nums[i]) {
+        if (count < 3) 
+        {
+            for (int i = 0; i < nums.size(); i++) 
+            {
+                if (max < nums[i]) 
+                {
                     max = nums[i];
                 }
             }
