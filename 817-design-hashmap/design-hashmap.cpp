458e7e77f45d72ff35d19564185904341b2bf509
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-01-16 11:15:23 +0530

    Time: 71 ms (23.21%) | Memory: 220.4 MB (19.36%) - LeetSync

--- 817-design-hashmap/design-hashmap.cpp
+++ 817-design-hashmap/design-hashmap.cpp
@@ -1,26 +1,26 @@
 class MyHashMap {
 public:
-    int arr[10000001];
+    vector<int>Hm;
 
     MyHashMap() 
     {
-       memset(arr,-1,sizeof(arr)) ;
+      Hm.resize(1000001,-1);
     }
     
     void put(int key, int value) 
     {
-        arr[key] = value;
+        Hm[key] = value;
     }
     
     int get(int key) 
     {
     
-        return arr[key];
+        return Hm[key];
     }
     
     void remove(int key) 
     {
-        arr[key] = -1;
+        Hm[key] = -1;
     }
 };
 
