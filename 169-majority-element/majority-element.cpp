4dc74ab216d0b11d785057d0898b8c86cc2c787a
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-01-30 21:14:02 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (58.55%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) 
+    {
+        int majority = nums[0];
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++)  // Standard loop with index
+        {
+            if(count == 0) 
+            {
+                majority = nums[i];  // Choose a new candidate
+            }
+
+            if(nums[i] == majority) 
+                count++;   // Increase count if it's the same as the majority
+            else 
+                count--;   // Decrease count if it's a different number
+        }
+
+        return majority;
+
+    }
+            
+};
