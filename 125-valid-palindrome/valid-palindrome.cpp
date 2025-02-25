fb97c1a656d84ce5bd3df95c857751284180e358
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-02-25 21:09:14 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (57.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -5,20 +5,28 @@ public:
     {
         int i=0;
         int j=s.size()-1;
+        
         while(i<=j)
         {
             while(i<s.size() && !iswalnum(s[i]))
-            {i++;
+            {
+                i++;
             }
+
             while(j>=0 && !iswalnum(s[j]))
-            {j--;
+            {
+                j--;
             }
+            
             if(i<=j && tolower(s[i])!=tolower(s[j]))
-            {return false;
+            {
+                return false;
             }
+
             i++;
             j--;  
         }
+        
         return true;
 
     }
