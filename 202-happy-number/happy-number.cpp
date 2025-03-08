714f4f499b457359f9f526296590f9be105a1c82
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-03-08 21:09:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (60.92%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    bool isHappy(int n) {
+    bool isHappy(int n) 
+    {
         if(n==1) return true;
 
         if(n<5 && n != 1) return false;
 
         int z = 0;
-        
+
         while(n>0)
         {
             z += pow( n%10 , 2 );
