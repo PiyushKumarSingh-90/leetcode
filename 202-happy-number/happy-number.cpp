dd2b0692600130e2783c8097805322a49c98ae04
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-03-08 21:08:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.06%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n==1) return true;
+
+        if(n<5 && n != 1) return false;
+
+        int z = 0;
+        
+        while(n>0)
+        {
+            z += pow( n%10 , 2 );
+            n = n/10;
+        }
+
+        return isHappy(z);
+    }
+};
