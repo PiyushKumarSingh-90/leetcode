c4c26ca52d743e8ebf253edfd3a1c1f089cdb2ab
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-03-13 21:02:42 +0530

    Time: 3 ms (70.38%) | Memory: 27.3 MB (73.13%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,35 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
-        if (inorder.size() != postorder.size())
-            return NULL;
 
-        map<int, int> hm;
+    int postIndex;  // Track current index in postorder traversal
+    unordered_map<int, int> indexMap;  // Store inorder indices for O(1) lookups
 
-        for (int i = 0; i < inorder.size(); ++i)
-            hm[inorder[i]] = i;
+    TreeNode* buildTreeUtil(vector<int>& inorder, vector<int>& postorder, int is, int ie) {
+        if (is > ie) return nullptr;
 
-        return buildTreePostIn(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1, hm);
-    }
+        TreeNode* root = new TreeNode(postorder[postIndex--]);
+        int inIndex = indexMap[root->val];  // O(1) lookup
 
-    TreeNode* buildTreePostIn(vector<int>& inorder, int is, int ie,
-                              vector<int>& postorder, int ps, int pe,
-                              map<int, int>& hm) {
-        if (ps > pe || is > ie) return NULL;
-
-        TreeNode* root = new TreeNode(postorder[pe]);
-
-        int inRoot = hm[postorder[pe]];
-        int numsLeft = inRoot - is;
-
-        root->left = buildTreePostIn(inorder, is, inRoot - 1, 
-                                     postorder, ps, ps + numsLeft - 1, hm);
-        
-        root->right = buildTreePostIn(inorder, inRoot + 1, ie, 
-                                      postorder, ps + numsLeft, pe - 1, hm);
+        // Build right subtree first (postorder processes right subtree before left)
+        root->right = buildTreeUtil(inorder, postorder, inIndex + 1, ie);
+        root->left = buildTreeUtil(inorder, postorder, is, inIndex - 1);
 
         return root;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) 
+    {
+        
+        postIndex = postorder.size() - 1;  // Start from last element of postorder
+        for (int i = 0; i < inorder.size(); i++)
+            indexMap[inorder[i]] = i;  // Store inorder indices for quick access
+
+        return buildTreeUtil(inorder, postorder, 0, inorder.size() - 1);
     }
 };
