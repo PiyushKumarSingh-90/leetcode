1e72b0315f12f134eb6ec2e014a6c0789a0addea
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-03-19 20:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (88.35%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -24,21 +24,24 @@ public:
 
         while(!q.empty())
         {
-            int n=q.size();
+            int n = q.size();
             vector<int>level;
 
-            for(int i=0;i<n;i++)
+            for(int i=0 ; i < n ;i++)
             {
-                TreeNode*data=q.front();
+                TreeNode* data = q.front();
+
                 q.pop();
+
                 level.push_back(data->val);
-                if(data->left!=NULL)
+
+                if(data->left != NULL)
                 {
-                 q.push(data->left);   
+                    q.push(data->left);   
                 }
-                if(data->right!=NULL)
+                if(data->right != NULL)
                 {
-                 q.push(data->right);   
+                    q.push(data->right);   
                 }
             }
             ans.push_back(level);
