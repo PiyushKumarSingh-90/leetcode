e2735e04e262225b9debc4f13047d405671072a1
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-07-22 15:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (75.13%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,7 +4,14 @@ public:
     {
         int count =0;
 
-       
+        // for(int i=0 ; i<nums.size(); i++)
+        // {
+        //     if(nums[i] != val)
+        //     {
+        //         nums[count] = nums[i];
+        //         count++;
+        //     }
+        // }
 
         int i=0;
 
@@ -15,6 +22,7 @@ public:
                 nums[count] = nums[i];
                 count++;
             }
+            
             i++;
         }
         return count;
