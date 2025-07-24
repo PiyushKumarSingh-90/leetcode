6010830de018fa24e49adff7926e89759e7150ee
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-07-24 18:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (50.91%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -6,15 +6,13 @@ public:
         if (nums.empty()) return 0; 
 
         int i = 0; 
-        int New = nums[0]; 
         
         for (int j = 1; j < nums.size(); j++) 
         {
-            if (nums[j] != New) 
+            if (nums[j] != nums[i]) 
             {
                 i++;  
                 nums[i] = nums[j]; 
-                New = nums[j]; 
             }
         } 
                
