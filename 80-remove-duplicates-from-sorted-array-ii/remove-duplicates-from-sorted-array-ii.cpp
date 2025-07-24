de468590cec1391a82587601931873540ca884f7
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-07-24 17:09:41 +0530

    Time: 13 ms (20.69%) | Memory: 19.4 MB (79.54%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,12 +1,16 @@
-class Solution {
+class Solution 
+{
 public:
     int removeDuplicates(vector<int>& nums) 
     {
-        if (nums.size() <= 2) return nums.size(); // Already valid
+        if (nums.size() <= 2) return nums.size(); 
 
-        int j = 2; // Position for next valid element
-        for (int i = 2; i < nums.size(); i++) {
-            if (nums[i] != nums[j - 2]) { // Allow max 2 occurrences
+        int j = 2; 
+
+        for (int i = 2; i < nums.size(); i++) 
+        {
+            if (nums[i] != nums[j - 2]) 
+            { 
                 nums[j] = nums[i];
                 j++;
             }
