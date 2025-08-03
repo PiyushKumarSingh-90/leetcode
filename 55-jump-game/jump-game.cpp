188c21b997da3813a7966046e9d352684894b728
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2025-08-03 20:26:51 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (98.97%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,7 +4,6 @@ public:
     bool canJump(vector<int>& nums) 
     {
         int jump = 0;
-
         
 
         for(int i = 0 ; i < nums.size() ; i++)
@@ -13,8 +12,7 @@ public:
 
             jump = max(jump , i + nums[i]);
 
-            if(jump >= nums.size()-1) return true ;
-            
+            if(jump >= nums.size()-1) return true ;   
         }
 
         return false;
