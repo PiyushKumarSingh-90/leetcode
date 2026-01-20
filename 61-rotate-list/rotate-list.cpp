623c9b938aaf3881990185badc3308a29d4c72dc
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-01-21 01:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (30.52%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -25,23 +25,22 @@ public:
         return n;
     }
 
+
     ListNode* rotateRight(ListNode* head, int k) 
-    {    
-           
+    {
         if(head == NULL) return head;
 
         int n = size(head);
 
-        int loop = k % n;
+        int loop = k % n ;
 
         loop = n - loop;
 
-        if( n == 1 || loop == n) return head;
+        if( n == 1 || n == loop) return head;
 
         int j = 0;
 
         ListNode* temp = head;
-        
         ListNode* firstAddress = head;
 
         while(temp != NULL)
@@ -54,20 +53,77 @@ public:
                 temp->next = NULL;
                 break;
             }
-            
+
             temp = temp->next;
         }
 
-        temp = firstAddress ;
+        temp = firstAddress;
 
         while(temp->next != NULL)
         {
-            temp = temp->next ;
+            temp = temp->next;
         }
 
         temp->next = head;
 
-        return firstAddress ;
+        return firstAddress;
 
     }
 };
+
+// int size(ListNode* head)
+//     {
+//         int n = 0;
+
+//         while(head != NULL)
+//         {
+//             n++;
+//             head = head->next;
+//         }
+
+//         return n;
+//     }
+
+//     ListNode* rotateRight(ListNode* head, int k) 
+//     {    
+           
+//         if(head == NULL) return head;
+
+//         int n = size(head);
+
+//         int loop = k % n;
+
+//         loop = n - loop;
+
+//         if( n == 1 || loop == n) return head;
+
+//         int j = 0;
+
+//         ListNode* temp = head;
+        
+//         ListNode* firstAddress = head;
+
+//         while(temp != NULL)
+//         {
+//             j++;
+
+//             if(j == loop)
+//             {
+//                 firstAddress = temp->next;
+//                 temp->next = NULL;
+//                 break;
+//             }
+            
+//             temp = temp->next;
+//         }
+
+//         temp = firstAddress ;
+
+//         while(temp->next != NULL)
+//         {
+//             temp = temp->next ;
+//         }
+
+//         temp->next = head;
+
+//         return firstAddress ;
