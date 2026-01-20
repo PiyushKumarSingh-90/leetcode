58e514b40f25d3bdfc14f96119614f03c0a115da
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-01-21 00:54:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (63.18%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -15,11 +15,13 @@ public:
     int size(ListNode* head)
     {
         int n = 0;
+
         while(head != NULL)
         {
             n++;
             head = head->next;
         }
+
         return n;
     }
 
@@ -28,25 +30,31 @@ public:
            
         if(head == NULL) return head;
 
-        int n= size(head);
+        int n = size(head);
+
         int loop = k % n;
+
         loop = n - loop;
 
-        if(n==1 || loop == n) return head;
+        if( n == 1 || loop == n) return head;
 
         int j = 0;
+
         ListNode* temp = head;
+
         ListNode* firstAddress = head;
 
         while(temp != NULL)
         {
             j++;
+
             if(j == loop)
             {
                 firstAddress = temp->next;
                 temp->next = NULL;
                 break;
             }
+            
             temp = temp->next;
         }
 
