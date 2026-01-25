61afe962b9cdce601f58c44d462e2f0f8ea6d45d
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-01-25 23:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (21.94%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,6 +1,7 @@
 class Solution 
 {
 public:
+
     int calculate(string s) 
     {
         stack<long long> st;
@@ -11,38 +12,95 @@ public:
         for (int i = 0; i < s.length(); i++) 
         {
             char ch = s[i];
-            
+
             if (isdigit(ch)) 
             {
                 num = num * 10 + (ch - '0');
-            } 
+            }
+
+
             else if (ch == '+' || ch == '-') 
             {
                 result += sign * num;
+
                 num = 0;
+
                 sign = (ch == '+') ? 1 : -1;
-            } 
+            }
+
             else if (ch == '(') 
             {
                 st.push(result);
                 st.push(sign);
+
                 result = 0;
                 sign = 1;
-            } 
+            }
+
             else if (ch == ')') 
             {
                 result += sign * num;
+
                 num = 0;
 
-                result *= st.top(); // sign
+                result *= st.top();
                 st.pop();
 
-                result += st.top(); // previous result
+                result += st.top();
                 st.pop();
             }
+
+
         }
 
         result += sign * num;
         return (int)result;
     }
 };
+
+//  stack<long long> st;
+
+//         long long result = 0, num = 0;
+//         int sign = 1;
+
+//         for (int i = 0; i < s.length(); i++)
+//         {
+//             char ch = s[i];
+
+//             if (isdigit(ch))
+//             {
+//                 num = num * 10 + (ch - '0');
+//             }
+
+//             else if (ch == '+' || ch == '-')
+//             {
+//                 result += sign * num;
+
+//                 num = 0;
+
+//                 sign = (ch == '+') ? 1 : -1;
+//             }
+
+//             else if (ch == '(')
+//             {
+//                 st.push(result);
+//                 st.push(sign);
+//                 result = 0;
+//                 sign = 1;
+//             }
+
+//             else if (ch == ')')
+//             {
+//                 result += sign * num;
+//                 num = 0;
+
+//                 result *= st.top(); // sign
+//                 st.pop();
+
+//                 result += st.top(); // previous result
+//                 st.pop();
+//             }
+//         }
+
+//         result += sign * num;
+//         return (int)result;
