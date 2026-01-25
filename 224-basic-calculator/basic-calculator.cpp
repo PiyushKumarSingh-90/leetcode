4bf3c1928c069512b1112cdf1fd49de5e94cecad
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-01-25 22:25:15 +0530

    Time: 3 ms (59.15%) | Memory: 13.7 MB (29.19%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,52 +1,48 @@
-class Solution {
+class Solution 
+{
 public:
     int calculate(string s) 
     {
-        stack<int> st;
-        int result = 0, num = 0, sign = 1; // sign = 1 for '+', -1 for '-'
+        stack<long long> st;
 
-        // Using a traditional for loop instead of range-based for loop
+        long long result = 0, num = 0;
+        int sign = 1;
+
         for (int i = 0; i < s.length(); i++) 
         {
-            char ch = s[i]; // Get the current character
+            char ch = s[i];
             
             if (isdigit(ch)) 
             {
-                num = num * 10 + (ch - '0'); // Build the number
+                num = num * 10 + (ch - '0');
             } 
             else if (ch == '+' || ch == '-') 
             {
-                result += sign * num; // Apply previous number
-                num = 0; // Reset num
-
-                // Update sign using if-else for better readability
-                if (ch == '+') 
-                {
-                    sign = 1;
-                } 
-                else 
-                {
-                    sign = -1;
-                }
+                result += sign * num;
+                num = 0;
+                sign = (ch == '+') ? 1 : -1;
             } 
             else if (ch == '(') 
             {
-                st.push(result);  // Store current result
-                st.push(sign);    // Store current sign
-                result = 0;       // Reset for new expression inside ()
-                sign = 1;         // Reset sign
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
             } 
             else if (ch == ')') 
             {
-                result += sign * num; // Apply last number inside ()
+                result += sign * num;
                 num = 0;
-                result *= st.top(); // Apply the sign before '('
+
+                result *= st.top(); // sign
                 st.pop();
-                result += st.top(); // Add the result before '('
+
+                result += st.top(); // previous result
                 st.pop();
             }
         }
 
-        return result + (sign * num); // Add last number to result
+        result += sign * num;
+        return (int)result;
     }
 };
