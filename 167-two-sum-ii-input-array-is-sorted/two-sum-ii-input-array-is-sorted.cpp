8ab5fc8cd42937e6f243b984bf0dd58baa76d4c5
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-01-31 20:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (99.83%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -30,28 +30,3 @@ public:
 };
 
 
-//  int left = 0, right = numbers.size() - 1;
-
-//         while (left < right) 
-//         {
-//             int sum = numbers[left] + numbers[right];
-
-//             if (sum == target) 
-//             {
-//                 return {left + 1, right + 1}; // 1-indexed result
-//             }
-
-//             if (sum < target) 
-//             {
-//                 left++;
-//             } 
-
-//             else 
-//             {
-//                 right--;
-//             }
-            
-//         }
-
-//         return {}; // This line is never reached as exactly one solution exists.
-//     }
