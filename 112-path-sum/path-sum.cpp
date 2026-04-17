d03ab7ddefcc6e205d0e56890e8f7d05c42e5c9d
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-04-18 01:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (66.74%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -13,11 +13,12 @@
 class Solution 
 {
 public:
+
     bool b = false;
 
     void preorder(int s, int targetSum, TreeNode* root)
     {
-        if (!root || b) return;   // ✅ stop if null OR already found
+        if (!root) return;   
 
         s += root->val;
 
@@ -26,7 +27,7 @@ public:
             if (s == targetSum)
             {
                 b = true;
-                return;          // ✅ stop further recursion
+                return;        
             }
         }
 
@@ -37,6 +38,7 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) 
     {
         preorder(0, targetSum, root);
+
         return b;
     }
 };
