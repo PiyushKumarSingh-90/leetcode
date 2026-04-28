47665afa01fed853a033c00f9f8e1d91c863ac4a
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-04-29 01:35:35 +0530

    Time: 3 ms (11.65%) | Memory: 23.8 MB (75.36%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
@@ -9,22 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution 
+{
 public:
     vector<double> averageOfLevels(TreeNode* root) 
     {
-        vector<double>avg;
-        queue<TreeNode*>q;
+        vector<double> avg;
+
+        queue<TreeNode*> q;
 
         q.push(root);
 
         while(!q.empty())
         {
             int n = q.size();
+
             double sum = 0;
             double count = n;
 
-            for(int i=0 ; i < n ; i++)
+            for(int i = 0 ; i < n ; i++)
             {
                TreeNode* temp = q.front();
 
@@ -42,9 +45,11 @@ public:
 
                 sum += temp->val;
             }
+
             avg.push_back(sum/count);
             
         }
+
         return avg;
     }
 };
