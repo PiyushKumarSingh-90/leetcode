a60b5c11afeb5783bd4e71551f829ec0bbe39937
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-08-06 00:09:44 +0530

    Time: 64 ms (73.08%) | Memory: 289.9 MB (45.19%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : invocations) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+
+        dfs(k, graph, suspicious);
+
+        for (int[] edge : invocations) {
+            if (!suspicious[edge[0]] && suspicious[edge[1]]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(int node, List<List<Integer>> graph, boolean[] suspicious) {
+
+        if (suspicious[node])
+            return;
+
+        suspicious[node] = true;
+
+        for (int next : graph.get(node)) {
+            dfs(next, graph, suspicious);
+        }
+    }
+}
