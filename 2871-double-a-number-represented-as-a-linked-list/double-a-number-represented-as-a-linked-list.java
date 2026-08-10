cee1e684bba90667e92ea26c4e782aa5e40e7eb5
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-08-10 21:11:25 +0530

    Time: 2 ms (100.00%) | Memory: 48.5 MB (48.40%) - LeetSync

---
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode doubleIt(ListNode head) {
+
+        // If first digit creates a carry
+        if (head.val > 4) {
+            head = new ListNode(0, head);
+        }
+
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            curr.val = (curr.val * 2) % 10;
+
+            // Carry from the next digit
+            if (curr.next != null && curr.next.val > 4) {
+                curr.val++;
+            }
+
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
