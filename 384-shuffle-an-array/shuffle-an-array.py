59cab57f0d9aec24f9169acc94e0d47c3da36277
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-09-11 17:52:14 +0530

    Time: 15 ms (86.89%) | Memory: 22.6 MB (46.27%) - LeetSync

--- 384-shuffle-an-array/shuffle-an-array.py
+++ 384-shuffle-an-array/shuffle-an-array.py
@@ -3,16 +3,18 @@ import random
 class Solution:
 
     def __init__(self, nums):
-        self.nums = nums
+        self.original = nums[:]
 
     def reset(self):
-        return self.nums
+        return self.original[:]
 
     def shuffle(self):
-        arr = self.nums[:]
+        arr = self.original[:]
 
-        for i in range(len(arr)):
-            j = random.randint(0, len(arr) - 1)
+        for i in range(len(arr) - 1, 0, -1):
+
+            j = random.randint(0, i)
+
             arr[i], arr[j] = arr[j], arr[i]
 
         return arr
