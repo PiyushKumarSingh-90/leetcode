31eb1f815799141ad046527f0a1db76c9c9cbbba
Piyush Kumar Singh <142776115+PiyushKumarSingh-90@users.noreply.github.com>
2026-09-11 17:58:56 +0530

    Time: 10 ms (24.97%) | Memory: 19.5 MB (5.44%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfExist(self, arr):
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+
+                if i != j and arr[i] == 2 * arr[j]:
+                    return True
+
+        return False
